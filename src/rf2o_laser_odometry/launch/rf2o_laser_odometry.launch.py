70ee252622f8172cda5ce8ad8994025439c10239
m0_51228868 <m0_51228868@noreply.gitcode.com>
2026-05-11 14:18:07 +0800

    Vendor rf2o_laser_odometry into repository

---
+++ src/rf2o_laser_odometry/launch/rf2o_laser_odometry.launch.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from launch.conditions import UnlessCondition
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+            Node(
+                package='rf2o_laser_odometry',
+                executable='rf2o_laser_odometry_node',
+                name='rf2o_laser_odometry',
+                output='screen',
+                parameters=[{
+                    'laser_scan_topic' : '/scan',
+                    'odom_topic' : '/odom_rf2o',
+                    'publish_tf' : False,
+                    'base_frame_id' : 'base_link',
+                    'odom_frame_id' : 'odom',
+                    'init_pose_from_topic' : '',
+                    'init_pose_from_imu_topic' : '/imu',
+                    'motion_filter_linear_m' : 0.01,
+                    'motion_filter_angular_rad' : 0.01,
+                    'freq' : 12.0}],
+            ),
+    ])
